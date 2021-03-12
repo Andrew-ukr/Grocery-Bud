2ede044cdc4de567d58f84d0d3d50b04248bee87
Andrii <pashulyaand@gmail.com>
2021-03-12 10:35:07 +0200

    done

--- src/App.js
+++ src/App.js
@@ -2,30 +2,57 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const checkStorage = () => {
+  let list = JSON.parse(localStorage.getItem("list"));
+  if (list) {
+    return list;
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(checkStorage());
   const [isEditing, setIsEditing] = useState(false);
-  // const [editId, setEditId] = useState(null);
+  const [editId, setEditId] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       showAlert();
+      console.log(1);
     }, 2000);
     return () => {
       clearTimeout(timeout);
+      console.log(1);
     };
-  }, [alert.show]);
+  }, [alert.show, list]);
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!name && !isEditing) {
+    if (!name) {
       showAlert(true, "please enter value", "danger");
     } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editId) {
+            return { ...item, name };
+          }
+          return item;
+        })
+      );
+      showAlert(true, "the value was edited", "success");
+      setName("");
+      setIsEditing(false);
+      setEditId(null);
     } else {
-      showAlert(true, "item added to the list", "success");
+      showAlert(true, "value added to the list", "success");
       const newItem = { id: new Date().getTime().toString(), name };
       setList([...list, newItem]);
       setName("");
@@ -39,6 +66,18 @@ function App() {
   const clearList = () => {
     showAlert(true, "the list is clear", "success");
     setList([]);
+  };
+
+  const delItem = (id) => {
+    showAlert(true, "item deleted", "danger");
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    const editId = list.find((item) => item.id === id);
+    setName(editId.name);
+    setIsEditing(true);
+    setEditId(editId.id);
   };
 
   return (
@@ -63,7 +102,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List list={list}></List>
+          <List list={list} delItem={delItem} editItem={editItem}></List>
           <button className="clear-btn" onClick={clearList}>
             clear item
           </button>
--- src/List.js
+++ src/List.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const List = ({ list }) => {
+
+const List = ({ list, delItem, editItem }) => {
   return (
     <div className="grocery-list">
-      {list.map(({id, name}) => {
+      {list.map(({ id, name }) => {
         return (
           <article key={id} className="grocery-item">
             <p className="title">{name}</p>
             <div className="btn-container">
-              <button className="edit-btn">
+              <button className="edit-btn" onClick={() => editItem(id)}>
                 <FaEdit></FaEdit>
               </button>
-              <button className="delete-btn">
+              <button className="delete-btn" onClick={() => delItem(id)}>
                 <FaTrash></FaTrash>
               </button>
             </div>
